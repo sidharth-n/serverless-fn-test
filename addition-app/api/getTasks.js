9092546ef44434a725e6ad4d879d24b15eb5e9d6
sidharth-n <sidharthnofficial@gmail.com>
2024-05-08 14:24:42 +0530

    Update require with import statement

--- addition-app/api/getTasks.js
+++ addition-app/api/getTasks.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb")
+import { MongoClient } from "mongodb"
 
 module.exports = async (req, res) => {
   const client = new MongoClient(process.env.MONGODB_URI, {
